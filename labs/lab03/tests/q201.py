ed205dacd3132266fc095aeeb9e4086cc030b6df
John DeNero <papajohn@gmail.com>
2016-02-03 07:37:08 -0800

    lab03-push

--- labs/lab03/tests/q201.py
+++ labs/lab03/tests/q201.py
@@ -6,8 +6,8 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> round(prob_no_shared_martian_birthdays, 6)
-          0.98551800000000001
+          >>> round(prob_no_shared_martian_birthdays, 4)
+          0.9855
           """,
           'hidden': False,
           'locked': False
