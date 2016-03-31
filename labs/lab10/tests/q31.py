c2cd0868095873045f2bd4ef8bab1b5bab603967
Henry Milner <henry@conviva.com>
2016-03-30 22:36:18 -0700

    lab10-push

--- labs/lab10/tests/q31.py
+++ labs/lab10/tests/q31.py
@@ -6,14 +6,15 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> c = make_best_knn_classifier(lyrics.take(range(20)), lyrics.take(range(20, 25)))
+          >>> your_classifier = make_best_knn_classifier(lyrics.take(range(20)), lyrics.take(range(20, 25)))
           >>> import collections
           >>> # make_best_knn_classifier should return a function.
-          >>> isinstance(c, collections.Callable)
+          >>> isinstance(your_classifier, collections.Callable)
           True
-          >>> # It looks like your classifier doesn't behave like the correct
-          >>> # one does.
-          >>> c(lyrics.drop(0).row(30)) == 'Hip-hop'
+          >>> # It looks like your function doesn't produce classifiers that
+          >>> # behave as expected.
+          >>> expected_classifier = make_knn_classifier(lyrics.take(range(20)), 1)
+          >>> np.all(lyrics.drop(0).apply(your_classifier) == lyrics.drop(0).apply(expected_classifier))
           True
           """,
           'hidden': False,
