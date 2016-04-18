540e813487eca1bac02047633874f36830b251b6
Henry Milner <henry@conviva.com>
2016-04-18 12:05:50 -0700

    project3-push

--- labs/project3/tests/q36.py
+++ labs/project3/tests/q36.py
@@ -6,12 +6,16 @@ test = {
       'cases': [
         {
           'code': r"""
+          >>> # Pay attention to what the sample size should be.
           >>> -1 <= simulate_statistic_under_null(1) <= 1
           True
+          >>> # Pay attention to what the sample size should be.
           >>> -1 <= simulate_statistic_under_null(1) <= 1
           True
+          >>> # Pay attention to what the sample size should be.
           >>> -1 <= simulate_statistic_under_null(1) <= 1
           True
+          >>> # Pay attention to what the sample size should be.
           >>> -150 <= simulate_statistic_under_null(1000) <= 150
           True
           """,
