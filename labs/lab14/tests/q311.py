ac220d0af31ee13f97c00af0e1d8acd1d5ae5f6b
Henry Milner <henry@conviva.com>
2016-04-26 22:12:20 -0700

    lab14-push

---
+++ labs/lab14/tests/q311.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> cancer
+          cancer status | negative | positive
+          healthy       | 9702     | 198
+          sick          | 10       | 90
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ labs/lab14/tests/q43.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> disease
+          disease status | negative | positive
+          disease        | 5        | 5
+          no disease     | 8991     | 999
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
