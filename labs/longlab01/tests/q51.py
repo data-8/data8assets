5763b8c533c0d54e90e2aa15dba44751bb9cc344
Henry Milner <henry@conviva.com>
2016-06-04 12:52:44 -0700

    longlab01-push

---
+++ labs/longlab01/tests/q51.py
@@ -0,0 +1,40 @@
+test = {
+  'name': 'Exercise 5.1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Remember to compute the absolute value of 7-10.  Traveling 
+          >>> # "-3 blocks" doesn't really make sense!
+          >>> num_avenues_away != -3
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> num_avenues_away
+          3
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> manhattan_distance
+          1462
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
