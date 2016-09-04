b607b69782116f37f6c96cc695a89c4b831a8bef
Henry Milner <henry@conviva.com>
2016-09-04 08:28:27 -0700

    fa16/hw/hw01.push

--- materials/fa16/hw/hw02/tests/q5_4.py
+++ materials/fa16/hw/hw02/tests/q5_4.py
@@ -8,14 +8,14 @@ test = {
           'code': r"""
           >>> sales.sort(0)
           box ID | fruit name | count sold | price per fruit ($)
-          3964   | grape      | 355        | 0.06
-          19930  | strawberry | 102        | 0.25
-          20532  | strawberry | 25         | 0.15
-          29093  | strawberry | 101        | 0.2
-          29161  | peach      | 17         | 0.8
-          42449  | apple      | 0          | 0.8
-          43118  | kiwi       | 3          | 0.5
-          52711  | orange     | 35         | 0.6
+          57930  | grape      | 355        | 0.06
+          52357  | strawberry | 102        | 0.25
+          26187  | strawberry | 25         | 0.15
+          57181  | strawberry | 101        | 0.2
+          43566  | peach      | 17         | 0.8
+          25274  | apple      | 0          | 0.8
+          53686  | kiwi       | 3          | 0.5
+          48800  | orange     | 35         | 0.6
           """,
           'hidden': False,
           'locked': False
