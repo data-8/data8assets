ea5b31cb9062e64b261f13df9955ad0f67221aee
Henry Milner <henry@conviva.com>
2016-09-04 18:32:33 -0700

    exercises/arrays/creating_arrays.push

---
+++ materials/exercises/arrays/creating_arrays/tests/q1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> weird_numbers
+          array([-2.        ,  0.93203909,  3.        ,  1.79174913])
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
