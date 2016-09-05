b2fe53511b468b5aec57571beeada0b360b522b2
Henry Milner <henry@conviva.com>
2016-09-04 18:35:47 -0700

    exercises/tables/sort/basic_sorting.push

---
+++ materials/exercises/tables/sort/basic_sorting/tests/q4.py
@@ -0,0 +1,27 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> len(pter)
+          90
+          >>> # It looks like you subtracted in the wrong order.
+          >>> round(pter.item(6), 4) != -1.1282
+          True
+          >>> round(pter.item(6), 4)
+          1.1282
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
