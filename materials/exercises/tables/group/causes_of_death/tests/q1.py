e3b8cebb48509bb4e7c3ffb5d4718e00ddf5db3e
Henry Milner <henry@conviva.com>
2016-09-04 18:34:51 -0700

    exercises/tables/group/causes_of_death.push

---
+++ materials/exercises/tables/group/causes_of_death/tests/q1.py
@@ -0,0 +1,27 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> top_5_causes
+          Cause of Death | Count
+          HTD            | 957108
+          CAN            | 822906
+          OTH            | 637764
+          STK            | 231897
+          CLD            | 194961
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
