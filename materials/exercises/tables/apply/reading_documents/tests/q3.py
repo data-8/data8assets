d71ce75e143539021339f03de94ece026d3c6f14
Henry Milner <henry@conviva.com>
2016-09-04 18:34:22 -0700

    exercises/tables/apply/reading_documents.push

---
+++ materials/exercises/tables/apply/reading_documents/tests/q3.py
@@ -0,0 +1,31 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> from datascience import *
+          >>> type(with_titles) == tables.Table
+          True
+          >>> with_titles.num_columns
+          2
+          >>> "Title" in with_titles.labels
+          True
+          >>> all(with_titles.column("Raw text") == reuters.column("Raw text"))
+          True
+          >>> with_titles.column("Title").item(4)
+          'NATIONAL AVERAGE PRICES FOR FARMER-OWNED RESERVE'
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/exercises/tables/apply/reading_documents/tests/q5.py
@@ -0,0 +1,31 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> from datascience import *
+          >>> type(with_dates) == tables.Table
+          True
+          >>> with_dates.num_columns
+          3
+          >>> "Date" in with_dates.labels
+          True
+          >>> all(with_dates.column("Raw text") == reuters.column("Raw text"))
+          True
+          >>> with_dates.column("Date").item(2000)
+          64
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
