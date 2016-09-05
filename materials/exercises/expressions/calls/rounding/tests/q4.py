60b0c9b3fff408acc7230bfe43a4756f94dc4977
Henry Milner <henry@conviva.com>
2016-09-04 18:33:26 -0700

    exercises/expressions/calls/rounding.push

---
+++ materials/exercises/expressions/calls/rounding/tests/q4.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> rounded_sum
+          72
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
