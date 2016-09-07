aa7a7a2290eca70f16e583e698d513c9c163f219
Henry Milner <henry@conviva.com>
2016-09-07 10:37:45 -0700

    fa16/hw/hw01.push

--- materials/fa16/lab/lab03/tests/q5_5.py
+++ materials/fa16/lab/lab03/tests/q5_5.py
@@ -6,9 +6,9 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> farmers_markets_with_fmid.num_columns
+          >>> farmers_markets_without_fmid.num_columns
           57
-          >>> sorted(farmers_markets_with_fmid.labels)
+          >>> sorted(farmers_markets_without_fmid.labels)
           ['Bakedgoods', 'Beans', 'Cheese', 'Coffee', 'County', 'Crafts', 'Credit', 'Eggs', 'Facebook', 'Flowers', 'Fruits', 'Grains', 'Herbs', 'Honey', 'Jams', 'Juices', 'Location', 'Maple', 'MarketName', 'Meat', 'Mushrooms', 'Nursery', 'Nuts', 'Organic', 'OtherMedia', 'PetFood', 'Plants', 'Poultry', 'Prepared', 'SFMNP', 'SNAP', 'Seafood', 'Season1Date', 'Season1Time', 'Season2Date', 'Season2Time', 'Season3Date', 'Season3Time', 'Season4Date', 'Season4Time', 'Soap', 'State', 'Tofu', 'Trees', 'Twitter', 'Vegetables', 'WIC', 'WICcash', 'Website', 'WildHarvested', 'Wine', 'Youtube', 'city', 'street', 'x', 'y', 'zip']
           
           """,
