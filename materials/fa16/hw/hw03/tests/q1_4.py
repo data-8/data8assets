887ff558e3154bffc0f4dfdb123df7932cd973bc
Henry Milner <henry@conviva.com>
2016-09-08 16:54:14 -0700

    fa16/hw/hw03.push

---
+++ materials/fa16/hw/hw03/tests/q1_4.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> round(revised_dissimilarity)
+          506.0
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
