0be7cae7bacb98fc130edf5ac9d2591db69e7c04
Henry Milner <henry@conviva.com>
2016-09-16 22:47:54 -0700

    exercises/tables/apply/predicting_temperatures.push

---
+++ materials/exercises/tables/apply/predicting_temperatures/tests/q3.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> round(thanksgiving_prediction, 2) == 64.32
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
