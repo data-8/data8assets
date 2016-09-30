c989e68a93b5d6e90a0f9e101acae7c4f18664bf
Henry Milner <henry@conviva.com>
2016-09-30 01:31:40 -0700

    exercises/probability/monkeys.push

---
+++ materials/exercises/probability/monkeys/tests/q1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> round(datascience_chance, 18) == 2.72e-16
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/exercises/probability/monkeys/tests/q6.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> round(e_chance, 4) == .3504
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
