02bb1cd567d6dd4a172a3c75e1eab33212669f16
Henry Milner <henry@conviva.com>
2016-09-30 01:31:37 -0700

    exercises/probability/how_many_devices.push

---
+++ materials/exercises/probability/how_many_devices/tests/q2.py
@@ -0,0 +1,24 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> int(num_required_for_50_percent) == num_required_for_50_percent
+          True
+          >>> abs(num_required_for_50_percent - 69) <= 11
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
