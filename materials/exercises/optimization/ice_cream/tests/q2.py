e21759d48785fa98eef1124b00e9ffa5ea131e6f
Henry Milner <henry@conviva.com>
2016-11-07 15:36:43 -0800

    exercises/optimization/ice_cream.push

---
+++ materials/exercises/optimization/ice_cream/tests/q2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> np.allclose(three_customers_mean_distance_from_925, 191+2/3)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
