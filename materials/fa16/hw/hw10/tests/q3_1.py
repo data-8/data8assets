8e2b0f9fbcfbccb148c40ba78d25c4cd9e53ea97
Henry Milner <henry@conviva.com>
2016-11-11 14:23:17 -0800

    fa16/hw/hw10.push

---
+++ materials/fa16/hw/hw10/tests/q3_1.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> import numpy as np
+          >>> np.allclose(three_customers_mean_distance_from_mean, 200)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/fa16/hw/hw10/tests/q3_4.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> round(average_distance(900)) == 570
+          True
+          >>> round(average_distance(1100)) == 533
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
