62f85cb02663abb3af638194d5c6bcf5c2865588
Henry Milner <henry@conviva.com>
2016-11-16 10:54:18 -0800

    fa16/hw/hw01.push

--- materials/fa16/project/project3/tests/q1_3_1_1.py
+++ materials/fa16/project/project3/tests/q1_3_1_1.py
@@ -10,6 +10,20 @@ test = {
           20
           >>> np.all([f in test_lyrics.labels for f in my_20_features])
           True
+          >>> # It looks like there are some songs in the training set that
+          >>> # don't have any of your chosen words.  That will make your
+          >>> # classifier perform very poorly in some cases.  Try choosing
+          >>> # at least 1 common word.
+          >>> train_f = train_lyrics.select(my_20_features)
+          >>> np.count_nonzero(train_f.apply(lambda r: np.sum(np.abs(np.array(r))) == 0)) < 150
+          True
+          >>> # It looks like there are some songs in the test set that
+          >>> # don't have any of your chosen words.  That will make your
+          >>> # classifier perform very poorly in some cases.  Try choosing
+          >>> # at least 1 common word.
+          >>> test_f = test_lyrics.select(my_20_features)
+          >>> np.count_nonzero(train_f.apply(lambda r: np.sum(np.abs(np.array(r))) == 0)) < 100
+          True
           """,
           'hidden': False,
           'locked': False
