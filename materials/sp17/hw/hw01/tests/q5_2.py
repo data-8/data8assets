c6f19aa71e6d5d7ec07b4391e1eb38cf3fda9a12
John DeNero <denero@berkeley.edu>
2017-01-20 23:09:49 -0800

    sp17/hw/hw01.push

---
+++ materials/sp17/hw/hw01/tests/q5_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 1 <= universities_q2 <= 3
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
