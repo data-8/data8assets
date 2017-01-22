d473e29eac474083e10439c9244df87204aa6889
John DeNero <denero@berkeley.edu>
2017-01-21 17:43:14 -0800

    sp17/hw/hw01.push

---
+++ materials/sp17/hw/hw01/tests/q3_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> four
+          4
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
