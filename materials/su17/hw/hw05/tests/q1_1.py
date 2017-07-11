b760b7dd73b0bc06660d1a70ef90b4ca0bd13844
Katherine Yen <katherine.yen@berkeley.edu>
2017-07-11 10:18:31 -0700

    su17/hw/hw05.push

---
+++ materials/su17/hw/hw05/tests/q1_1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 0 < p_winning_after_two_flips < 1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/su17/hw/hw05/tests/q1_6.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 1_6',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 0 <= p_value < 1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
