969cbcd2655d53b19a31b959ab2a72d087a089ac
Nanxi Wang <nwang97@berkeley.edu>
2017-07-25 16:34:34 -0700

    su17/lab/lab08.push

---
+++ materials/su17/lab/lab08/tests/q1_6.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> "%.2f" % round(r,2)
+          '0.90'
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
